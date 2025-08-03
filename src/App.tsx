5f7c9143954427cc36e77b0f6bfad3d38091305d
lihaicheng <lihaicheng.email@gmail.com>
2025-08-03 19:48:30 +0800

    React module components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import robots from './mockdata/robots.json';
+import Robot from './components/Robot';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
+      <div className={styles.robotList}>
+        {robots.map(r => <Robot id={r.id} email={r.email} name={r.name} />)}
+      </div>
     </div>
   );
 }
