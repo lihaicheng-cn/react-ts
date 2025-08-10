b72a2ccfba0a69b1c67d7ec3feca44b124881ce6
lihaicheng <lihaicheng.email@gmail.com>
2025-08-10 22:58:33 +0800

    use useState hook to manage component state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './assets/images/logo.svg';
 // import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
@@ -12,55 +12,31 @@ interface State {
   count: number;
 }
 
-class App extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      robotGallery: [],
-      count: 0,
-    };
-  }
+const App: React.FC = (props) => {
+  const [count, setCount] = useState<number>(0);
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((data) => this.setState({ robotGallery: data }));
-  }
-
-  render() {
-    return (
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
-        </div>
-        <button
-          onClick={() => {
-            this.setState(
-              (preState, preProps) => {
-                return { count: preState.count + 1 };
-              },
-              () => {
-                console.log('count ', this.state.count);
-              }
-            );
-            // this.setState({ count: this.state.count + 1 }, () => {
-            //   console.log('count ', this.state.count);
-            // });
-          }}
-        >
-          Click
-        </button>
-        <span>count: {this.state.count}</span>
-        <ShoppingCart></ShoppingCart>
-        <div className={styles.robotList}>
+  return (
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>罗伯特机器人炫酷吊炸天online购物平台的名字要长</h1>
+      </div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click
+      </button>
+      <span>count: {count}</span>
+      <ShoppingCart></ShoppingCart>
+      {/* <div className={styles.robotList}>
           {this.state.robotGallery.map((r) => (
             <Robot id={r.id} email={r.email} name={r.name} />
           ))}
-        </div>
-      </div>
-    );
-  }
-}
+        </div> */}
+    </div>
+  );
+};
 
 export default App;
