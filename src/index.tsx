cc5ea183119939249ce4eecea5d391ac709cf556
lihaicheng <lihaicheng.email@gmail.com>
2025-08-19 11:31:53 +0800

    componentize context provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const defaultContextValue = {
-  username: '阿莱克斯',
-};
-
-export const appContext = React.createContext(defaultContextValue);
+import { AppStateProvider } from './AppState';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <appContext.Provider value={defaultContextValue}>
+    <AppStateProvider>
       <App />
-    </appContext.Provider>
+    </AppStateProvider>
   </React.StrictMode>
 );
 
